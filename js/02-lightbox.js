fb81356d77d83d34330e86ef11c3cb64458863e5
Andrii Nahornyi <navamster@gmail.com>
2022-09-12 16:12:54 +0300

    Update 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Variables
 const galleryEl = document.querySelector('.gallery');
 
 const markupGallery = createGalleryItemsMarkup(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", markupGallery);
+
 // Listeners
 galleryEl.addEventListener('click', targetClickHandler);
+
 // Functions
-// 3. Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+// 3. Initializing the library after creation & додання gallery items to 'div.gallery'.
 // (SimpleLightbox: «Usage», «Markup»).
 // *instance SimpleLightBox:
 // var lightbox = new SimpleLightbox('.gallery a', { /* options */ }); or
 // var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
-// 4. SimpleLightbox («Options»): як додати відображення підписів до зображень з атрибута alt. Підпис має бути знизу і з'являєтись через 250 мілісекунд після відкриття зображення.
-// Options: captionsData: get the caption from given attribute; 
+// 4. SimpleLightbox («Options»): add IMG captions from given attribute 'alt'; captions position: bottom; caption delay: 250 ms after open the IMG.
+// Options:  captionsData: get the caption from given attribute; 
 // captionPosition: top, bottom or outside;
 // captionDelay: adds a delay before the caption shows(in ms);
+
 function targetClickHandler(event) {
     event.preventDefault();
 }
+
 let lightbox = new SimpleLightbox('.gallery a', { 
     captionsData:	'alt',
     captionPosition: 'bottom',
     captionDelay: 250,
- });
+});
+ 
 // 1. Create & render markup
 function createGalleryItemsMarkup(items) {
     const galleryItemsMarkup = items.map(({ preview, original, description }) => `<a class="gallery__item" href='${original}'>
@@ -36,4 +42,3 @@ function createGalleryItemsMarkup(items) {
     console.log(galleryItemsMarkup);
     return galleryItemsMarkup;
 }  
-// console.log(galleryItems);
